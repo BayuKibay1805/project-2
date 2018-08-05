4130527e61ebc67c5bec2f22d040883b085be405
BayuKibay1805 <42013200+BayuKibay1805@users.noreply.github.com>
2018-08-05 19:56:31 +0700

    Update shard.js

--- shard.js
+++ shard.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const Manager = new Discord.ShardingManager('./server.js', {
   token: process.env.TOKEN
 })
-Manager.spawn(2); // This example will spawn 2 shards (5,000 guilds);
+Manager.spawn(1); // This example will spawn 2 shards (5,000 guilds);
 
 Manager.on('launch', shard => {
   console.log(`[${new Date().toISOString()}] Launched shard #${shard.id}`)
